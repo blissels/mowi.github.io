2c9c6111aecf5139fde47b3910572bbd48883989
blissels <urdnanat@gmail.com>
2024-01-04 19:12:25 +0700

    first commit

---
+++ scriptvid.js
@@ -0,0 +1,7 @@
+document.getElementById('openVideo').addEventListener('click', function() {
+    document.getElementById('videoPopup').style.display = 'block';
+});
+
+document.getElementById('closeVideo').addEventListener('click', function() {
+    document.getElementById('videoPopup').style.display = 'none';
+});
